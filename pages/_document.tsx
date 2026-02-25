86b6c67091ad208dbcd5a6a23e15a1da37b25d60
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-02-26 01:15:20 +0900

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,28 @@ export default function Document() {
   console.log("DOCUMENT - PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Petora-next" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="img/png" href="/img/logo/Favicon.png" />
+
+        {/* SEO */}
+        <meta
+          name="keyword"
+          content={
+            "petora, pet, pet.uz, pet.kr, pets.kr, pets, pets.uz, petcare, mern nestjs fullstack"
+          }
+        />
+        <meta
+          name="description"
+          content={
+            "Buy pet products and Use pet services anywhere anytime in South Korea. | " +
+            "Покупайте товары для домашних животных и пользуйтесь услугами для животных в любом месте и в любое время в Южной Кореи. | " +
+            "한국 어디에서든 반려동물 용품을 구매하고 반려동물 서비스를 이용하세요."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
