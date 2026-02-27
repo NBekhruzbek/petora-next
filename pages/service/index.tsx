6c8fb22e76e11d43530f7e73d37a507c46aaaf31
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-02-28 08:06:42 +0900

    feat: publish ServicePage Header

--- pages/service/index.tsx
+++ pages/service/index.tsx
@@ -1,9 +1,9 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Service: NextPage = () => {
-  return <Container>Services Page</Container>;
+  return <Stack className="container">Services Page</Stack>;
 };
 
 export default withLayoutBasic(Service);
